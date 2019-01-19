5619cab374c20c69807e1ebf933dd93f1f8cd83d
Greg Morgan <gregm9999@gmail.com>
2019-01-19 19:09:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let store={drivers:[], passengers:[], trips:[]};
+let store = {drivers:[], passengers:[], trips:[]}
 let driverId=0;
 let passengerId=0;
 let tripId=0;
@@ -7,53 +7,53 @@ class Driver {
   constructor(name) {
     this.name=name;
     this.id=driverId++;
-    store.drivers.push(this)
+    store.drivers.push(this);
   }
   trips() {
     return store.trips.filter(trip => {
       return trip.driverId===this.id;
-    });
+    })
   }
   passengers() {
     return this.trips().map(trip => {
       return trip.passenger();
-    })
-
-  }
+  })
+}
 }
 
 class Passenger {
   constructor(name) {
     this.name=name;
     this.id=passengerId++;
-    store.passengers.push(this)
+    store.passengers.push(this);
   }
   trips() {
     return store.trips.filter(trip => {
-      return trip.passengerId === this.id;
+      return trip.passengerId===this.id;
     })
   }
   drivers() {
     return this.trips().map(trip => {
-      return trip.driver()
-    })}
+      return trip.driver();
+    })
+  }
 }
 
 class Trip {
   constructor(driver, passenger) {
+    this.id=tripId++;
     this.driverId=driver.id;
     this.passengerId=passenger.id;
-    this.id=tripId++;
     store.trips.push(this);
   }
   driver() {
-    return store.drivers.find(driver=> {
+    return store.drivers.find(driver => {
       return driver.id===this.driverId;
     })
   }
   passenger() {
     return store.passengers.find(passenger => {
       return passenger.id===this.passengerId;
-    });
+    })
   }
 }
